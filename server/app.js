7ddbabca85b7c6e33e5613d0e6883ef8f31d18eb
U_JINYEOL <100745412+U-Jinyeol@users.noreply.github.com>
2024-02-05 17:20:32 +0900

    [FIX] 배포 테스트

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://snack-script.vercel.app",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
