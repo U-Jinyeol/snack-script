f68fcff44d82568e8f0f3a0c5fd6eeb10933f06b
Milan Lee <100390926+doremilan@users.noreply.github.com>
2024-02-07 21:40:36 +0900

    [FIX] morgan 위치 조정

--- server/app.js
+++ server/app.js
@@ -22,11 +22,11 @@ app.use(express.json());
 app.use('/api', api);
 connect();
 
-app.use(morgan('combined'));
-
 app.use(function (err, _, res, __) {
   res.status(500).json({ ok: false, message: err.message });
 });
+
+app.use(morgan('combined'));
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
