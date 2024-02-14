40133f7bb4729a3346ef792cd4616c819a3d5c71
U_JINYEOL <100745412+U-Jinyeol@users.noreply.github.com>
2024-02-14 12:07:19 +0900

    [FEAT] 로딩 컴포넌트

---
+++ src/components/Common/Loading/index.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const Loading = () => {
+  return (
+    <div className="loader">
+      <span className="loader-text">loading</span>
+      <span className="load"></span>
+    </div>
+  );
+};
+
+export default Loading;
